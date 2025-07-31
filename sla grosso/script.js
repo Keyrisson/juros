0b67cc28577798ca90ebff49501fcb0dd2c73eac
Keyrisson <keyrisson.sibert@escola.pr.gov.br>
2025-07-31 11:12:11 -0300

    Add files via upload

---
+++ sla grosso/script.js
@@ -0,0 +1,13 @@
+function calculo(){
+
+    let C = parseInt(document.getElementById('primeiro_numero').value);
+    let I = parseInt(document.getElementById('segundo_numero').value);
+    let T = parseInt(document.getElementById('terceiro_numero').value);
+    I = I/100;
+    let soma_numero = C * I * T;
+    document.getElementById("resultado").textContent= 'total dos juros é: ' + soma_numero;
+    let M = parseInt(document.getElementById('quarto_numero').value);
+    M = soma_numero + C;
+    let soma_numero2 = M;
+    document.getElementById("quarto_numero").textContent= 'e o montante:' + soma_numero2;
+}
